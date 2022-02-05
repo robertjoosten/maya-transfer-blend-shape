f9174f13ca4ef3d5cdd4a544c5b02505d9c2850f
Robert Joosten <rwm.joosten@gmail.com>
2022-02-05 13:38:27 +0000

    exit with error code

--- tests/bin/run.py
+++ tests/bin/run.py
@@ -1,15 +1,17 @@
 def main():
+    import sys
     import unittest
     import maya.standalone
 
     maya.standalone.initialize()
 
-    test_runner = unittest.TextTestRunner(verbosity=3)
+    test_runner = unittest.TextTestRunner(verbosity=2)
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover("tests", pattern="test_*.py")
-    test_runner.run(test_suite)
+    result = test_runner.run(test_suite)
 
     maya.standalone.uninitialize()
+    sys.exit(int(not result.wasSuccessful()))
 
 
 if __name__ == "__main__":
